fa6087f04f7486209d5b14ee9458e0eb79a79d59
shirai <shirai@n-i-agroinformatics.com>
2012-10-29 23:28:51 +0900

    ejsテンプレート追加

--- public/js/dfm.js
+++ public/js/dfm.js
@@ -1,12 +1,25 @@
 $(function(){
+
+		// 欠席者リスト表示
+		friend = $(new EJS({
+				url: "ejs/friend.ejs"
+		}).render());
+		$("#friend-container").prepend(friend);
+
+		// フレームリスト表示
+		frame = $(new EJS({
+				url: "ejs/frame.ejs"
+		}).render());
+		$("#frame-container").prepend(frame);
+		
+		// 欠席者追加
 		$(".friend-wrapper").click(function(){
-				$("#absence-container").prepend("<div class='absence-wrapper' style='position: absolute; top: 5px; left: 5px;'><img src='img/frame_picture.jpg' class='square' style='width: 100px; height: 130px;' /><div class='controll-wrapper' style='position: absolute;top: 0px;left: 0px;'><span style='font-size: 20px;font-weight: bold;background: whitesmoke;border-radius: 10px;width: 20px;height: 20px;position: absolute;text-align: center;line-height: 15px;'><a href='#' style='text-decoration: none;' onclick='$(this).closest(\".absence-wrapper\").remove(); return false;'>×</a></span></div></div>");
+				absence = $(new EJS({
+          url: "ejs/absence.ejs"
+        }).render());
+				$("#absence-container").prepend(absence);
 				$(".absence-wrapper").draggable();
-				//$(".absence-wrapper").click(function(){
-				//		$(this).remove();
-				//});
 		});
 		
 		$("#frame-container").selectable();
-
 });
