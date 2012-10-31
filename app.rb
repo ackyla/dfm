b0aea8e6c9010aa7f2c9d67fe918de58be7d25ab
shirai <shirai@n-i-agroinformatics.com>
2012-11-01 02:50:14 +0900

    友達取得まで

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class DfmApp < Sinatra::Base
 
   get '/auth' do
     auth = FbGraph::Auth.new APP_KEY, APP_SECRET, :redirect_uri => "#{request.scheme}://#{request.host}:#{request.port}/auth/callback"
-    redirect auth.client.authorization_uri()
+    redirect auth.client.authorization_uri(:scope => [:email])
   end
 
   get '/auth/callback' do
@@ -30,7 +30,12 @@ class DfmApp < Sinatra::Base
     auth.client.authorization_code = params[:code]
     access_token = auth.client.access_token! :client_auth_body
     user = FbGraph::User.me(access_token).fetch
-    user.name
+    friends = user.friends({"locale" => "ja_JP"})
+    names = ""
+    friends.each do |friend|
+      names += "<img style='border: 1px solid black;' src='#{friend.picture({"&width=" => "34", "&height=" => "34"})}' /> #{friend.name}<br />"
+    end
+    names
     #redirect '/edit'
   end
   
