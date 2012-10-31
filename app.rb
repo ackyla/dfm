90254b2dea70a54a0535f769f83934ff5cfdbdef
shirai <shirai@n-i-agroinformatics.com>
2012-11-01 01:03:56 +0900

    facebookのユーザ情報とれるところまで

--- app.rb
+++ app.rb
@@ -1,14 +1,43 @@
 # -*- coding: utf-8 -*-
-require 'sinatra'
+require 'sinatra/base'
+require 'fb_graph'
 
-get '/' do
-  erb :index
-end
+class DfmApp < Sinatra::Base
+  
+  configure do
+    APP_KEY, APP_SECRET = File.open(".key").read.split
+  end
 
-get '/edit' do
-  erb :edit
-end
+  error do
+    "sorry"
+  end
+  
+  not_found do
+    "404"
+  end
+  
+  get '/' do
+    erb :index
+  end
 
-post '/create' do
-  params[:x]
+  get '/auth' do
+    auth = FbGraph::Auth.new APP_KEY, APP_SECRET, :redirect_uri => "#{request.scheme}://#{request.host}:#{request.port}/auth/callback"
+    redirect auth.client.authorization_uri()
+  end
+
+  get '/auth/callback' do
+    auth = FbGraph::Auth.new APP_KEY, APP_SECRET, :redirect_uri => "#{request.scheme}://#{request.host}:#{request.port}/auth/callback"
+    auth.client.authorization_code = params[:code]
+    access_token = auth.client.access_token! :client_auth_body
+    user = FbGraph::User.me(access_token).fetch
+    user.name
+    #redirect '/edit'
+  end
+  
+  get '/edit' do
+    erb :edit
+  end
+
+  post '/create' do
+  end
 end
