666faef7e48912d58e79d4c86bb852c2ba969069
shirai <shirai@n-i-agroinformatics.com>
2012-11-02 01:07:55 +0900

    本物の友達リスト表示

--- public/js/dfm.js
+++ public/js/dfm.js
@@ -1,30 +1,33 @@
 $(function(){
 
 		// 欠席者リスト表示
-		friend = $(new EJS({
-				url: "ejs/friend.ejs"
-		}).render());
-		$("#friend-container").prepend(friend);
-
+		$.getJSON("http://localhost:9393/friends.json", function(json){
+				friend = $(new EJS({
+						url: "ejs/friend.ejs"
+				}).render({ friends: json }));
+				$("#friend-container").append(friend);
+		});
+		
 		// フレームリスト表示
 		frame = $(new EJS({
 				url: "ejs/frame.ejs"
 		}).render({ shapes: ["square", "oval", "square", "square", "square", "oval", "oval"] }));
 		$("#frame-container").prepend(frame);
 		
-		// 欠席者追加
-		$(".friend-wrapper").click(function(){
-				absence = $(new EJS({
-          url: "ejs/absence.ejs"
-        }).render());
-				$("#absence-container").prepend(absence);
-				$(".absence-wrapper").draggable({
-						drag: function(){
-								$(this).find("input.position-x").val($(this).css("left"));
-								$(this).find("input.position-y").val($(this).css("top"));
-						}
-				});
-		});
-		
 		$("#frame-container").selectable();
+
 });
+
+function addAbsence() {
+		// 欠席者追加
+		absence = $(new EJS({
+        url: "ejs/absence.ejs"
+    }).render());
+		$("#absence-container").prepend(absence);
+		$(".absence-wrapper").draggable({
+				drag: function(){
+						$(this).find("input.position-x").val($(this).css("left"));
+						$(this).find("input.position-y").val($(this).css("top"));
+				}
+		});
+}
