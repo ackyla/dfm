085b399ac5446ecf29c6ae2c82f3cd2596e7b102
shirai <shirai@n-i-agroinformatics.com>
2013-03-26 20:18:37 +0900

    test

--- app.rb
+++ app.rb
@@ -375,23 +375,35 @@ class DfmApp < Sinatra::Base
   # Return:: json
   get '/albums.json' do
     user = FbGraph::User.me(session[:token]).fetch({"locale" => "ja_JP"})
-    albums = user.albums({"locale" => "ja_JP", "offset" => params[:offset], :fields => "id, name, cover_photo"})
+    begin
+      albums = user.albums({"locale" => "ja_JP", "offset" => params[:offset], :fields => "id, name, cover_photo"})
+    rescue
+      halt(500)
+    end
     offset = albums.next.empty? ? nil : (params[:offset].to_i + albums.count)
-    albums = albums.to_a.map{|album|
-      {
-        "id" => album.identifier,
-        "name" => album.name,
-        "source" => album.cover_photo.nil? ? nil : album.cover_photo.fetch({:access_token => session[:token], :fields => "source"}).source,
+    begin
+      albums = albums.to_a.map{|album|
+        {
+          "id" => album.identifier,
+          "name" => album.name,
+          "source" => album.cover_photo.nil? ? nil : album.cover_photo.fetch({:access_token => session[:token], :fields => "source"}).source,
+        }
       }
-    }
+    rescue
+      halt(404)
+    end
 
-    #if(params[:offset].to_i == 0)
-    #  photos = user.photos({"type" => "tagged", "limit" => 1, :fields => "source"})
-      # 自分が写っている写真が1枚以上あった時はアルバムとして表示する
-   #   if(photos.count > 0)
-   #     albums.unshift({"id" => 0, "name" => "あなたが写っている写真", "source" => photos[0].source, "tags" => Array::new})
-   #   end
-   # end
+    begin
+      if(params[:offset].to_i == 0)
+        photos = user.photos({"type" => "tagged", "limit" => 1, :fields => "source"})
+        # 自分が写っている写真が1枚以上あった時はアルバムとして表示する
+        if(photos.count > 0)
+          albums.unshift({"id" => 0, "name" => "あなたが写っている写真", "source" => photos[0].source, "tags" => Array::new})
+        end
+      end
+    rescue
+      halt(401)
+    end
 
 
     response = {
